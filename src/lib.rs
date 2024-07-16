8f79c46063082cca9c34ab183e4f66f44b09a62a
miampf <miampf@proton.me>
2024-07-16 18:20:41 +0000

    feat: initial implementation (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+pub mod graph;
 pub mod plugin;
+pub mod types;
+
+mod test {
+    use crate::plugin::{NewState, Wrapper};
+
+    #[test]
+    fn test_plugin() {
+        // TODO: once some plugin is written, write an e2e test as well
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use petgraph::dot::{Config, Dot};
+
+use schnuffel::graph::{Graph, Node};
+use schnuffel::plugin::Wrapper;
+use schnuffel::types::{EmailAddress, PhoneNumber};
+
 fn main() {
-    println!("Hello, world!");
+    let plugin = Wrapper::load(
+        "https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm",
+    );
 }
