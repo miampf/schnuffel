511f71a12e60d9844f16db8e57d1f6b6b032e608
miampf <miampf@proton.me>
2024-07-21 14:14:22 +0000

    graph: extract `update_graph` and adjust constants (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use iced::executor;
-use iced::mouse::ScrollDelta;
 use iced::theme::Theme;
 use iced::Settings;
 use iced::{Application, Command, Element};
-use schnuffel::views::graph::{self, GraphState, GraphStateUpdate};
-use schnuffel::views::ViewState;
+use schnuffel::views::graph::helpers::update_graph;
+use schnuffel::views::graph::GraphState;
 use schnuffel::Message;
 
 // all of our views
@@ -55,76 +54,6 @@ impl Application for App {
     fn theme(&self) -> Self::Theme {
         Theme::Light
     }
-}
-
-fn update_graph(state: &mut GraphState, message: Message) {
-    match message {
-        Message::MouseClick(position) => {
-            for node in &mut state.graph.nodes {
-                // clear selections
-                node.is_selected = false;
-
-                if (position.x - node.x).powf(2.0) + (position.y - node.y).powf(2.0)
-                    < (node.radius * state.zoom_factor).powf(2.0)
-                {
-                    node.is_dragged = true;
-                    node.is_selected = true;
-                }
-            }
-        }
-        Message::MouseDrag(position) => {
-            for node in &mut state.graph.nodes {
-                if node.is_dragged {
-                    node.x = position.x;
-                    node.y = position.y;
-                }
-
-                if state.is_panning {
-                    let pan_delta = state.panning_start_point - position;
-                    node.x -= pan_delta.x * graph::constants::PAN_MULTIPLIER;
-                    node.y -= pan_delta.y * graph::constants::PAN_MULTIPLIER;
-                }
-            }
-            if state.is_panning {
-                state.panning_start_point = position;
-            }
-        }
-        Message::MouseRelease => {
-            state.is_panning = false;
-            for node in &mut state.graph.nodes {
-                node.is_dragged = false;
-            }
-        }
-        Message::MouseScroll(delta) => {
-            if let ScrollDelta::Lines { x: _, y } = delta {
-                state.zoom_factor += y * graph::constants::ZOOM_MULTIPLIER;
-                state.zoom_factor = state
-                    .zoom_factor
-                    .clamp(graph::constants::MIN_ZOOM, graph::constants::MAX_ZOOM);
-
-                for node in &mut state.graph.nodes {
-                    // update the node positions to reflect the zoom
-                    if y < 0.0 && state.zoom_factor != graph::constants::MIN_ZOOM {
-                        // zoom out
-                        node.x += state.zoom_factor * graph::constants::NODE_ZOOM_POSITION_CHANGE;
-                        node.y += state.zoom_factor * graph::constants::NODE_ZOOM_POSITION_CHANGE;
-                    } else if y > 0.0 && state.zoom_factor != graph::constants::MAX_ZOOM {
-                        // zoom in
-                        node.x -= state.zoom_factor * graph::constants::NODE_ZOOM_POSITION_CHANGE;
-                        node.y -= state.zoom_factor * graph::constants::NODE_ZOOM_POSITION_CHANGE;
-                    }
-                }
-            }
-        }
-        Message::MiddleMouseClick(position) => {
-            state.is_panning = true;
-            state.panning_start_point = position;
-        }
-    };
-    state.update_state(GraphStateUpdate {
-        graph: state.graph.clone(),
-        zoom_factor: state.zoom_factor,
-    });
 }
 
 fn main() -> iced::Result {
